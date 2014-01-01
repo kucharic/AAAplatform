6ed47bcaba3ee582603915112932d05912f8a747
kucharic <kucharic@fel.cvut.cz>
2014-01-01 13:01:13 +0100

    kerberos-confgiure script initial commit

---
+++ .install/kerberos-configure.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#test if need to be run
+if [ -f "${0}.done" ] ; then
+    echo "Kerberos alreade installed."
+    exit 0
+fi
+
+. ${0%/*}/../bin/aaa.lib
+
+#configure installation
+cat <<KRB_PRESEED | debconf-set-selections
+krb5-admin-server krb5-admin-server/kadmind boolean true
+krb5-config krb5-config/add_servers_realm string KME.FEL.CVUT.CZ
+krb5-config krb5-config/read_conf boolean true
+krb5-config krb5-config/default_realm string KME.FEL.CVUT.CZ
+krb5-config krb5-config/add_servers boolean true
+krb5-kdc krb5-kdc/debconf boolean true
+krb5-kdc krb5-kdc/purge_data_too boolean false
+KRB_PRESEED
+
+#install
+install krb5-admin-server krb5-config krb5-kdc
+
+for i in /etc/krb5* ; do
+    mv /etc/$i /etc/$i.bfaaa
+tar -xzf $path/kerberos-configure/kerberos.tgz -C /etc/
+cp /etc/krb5kdc.bfaaa/stash /etc/krb5kdc/
+
+/etc/init.d/krb5-admin-server restart
+/etc/init.d/krb5-kdc restart
+
+#policies
+kadmin.local -q 'add_policy -minlength 4 -minclasses 1 admin'
+kadmin.local -q 'add_policy -minlength 3 -minclasses 1 host'
+kadmin.local -q 'add_policy -minlength 3 -minclasses 1 service'
+kadmin.local -q 'add_policy -minlength 3 -minclasses 1 user'
+
+#disable using
+touch "${0}.done"
+exit 0
