7810090a91bee3818524166e0008502d099951b6
kucharic <kucharic@fel.cvut.cz>
2014-01-01 13:00:51 +0100

    psql-configure bugfixes

--- .install/psql-configure.sh
+++ .install/psql-configure.sh
@@ -10,15 +10,15 @@ fi
 
 #install
 install postgresql
-if ! $? ; then
+if (( $? != 0 )) ; then
     echo "Resolve issue and install again!"
     exit 1
 fi
 
 echo 'local all all trust' >> /etc/postgresql/*/main/pg_hba.conf
-su - postgres
-echo "CREATE USER root WITH PASSWORD 'pass';" | psql template1
-logout
+/etc/init.d/postgresql restart
+
+su - postgres -c "psql template1 < $path/psql-configure.pgsql"
 
 #disable using
 touch "${0}.done"
